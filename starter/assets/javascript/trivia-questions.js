ebfb7855228304344ee62dc3b5872fded9d57e55
JayClay922 <jamesclayman922@live.com>
2023-01-13 18:58:27 +0000

    updating code to let first question and it's answer options be seen in the DOM with the answer options being in buttons

--- starter/assets/javascript/trivia-questions.js
+++ starter/assets/javascript/trivia-questions.js
@@ -1,41 +1,41 @@
 let myTriviaQuestions = [{
-    question: "Inside which HTML element do we put the Javascript?",
+    question: "1. Inside which HTML element do we put the Javascript?",
     answers: ["<scripting>", "<script>", "<js>", "<javascript>"]           
 },
 {
-    question: "How do you write 'Hello World' in an alert box",
+    question: "2. How do you write 'Hello World' in an alert box",
     answers: ["alert('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", "msg('Hello World');"]
 },
 {
-    question: "How do you write an IF statement in Javascript?",
+    question: "3. How do you write an IF statement in Javascript?",
     answers: ["if i == 5 then", "if i = 5", "if i = 5 then", "if(i == 5)"]
 },
 {
-    question: "How does a FOR loop start?",
+    question: "4. How does a FOR loop start?",
     answers: ["for i = 1 to 5", "for(i <= 5; i++", "for(i = 0; i <= 5)", "for(i = 0; i <= 5; i++)"]
 },
 {
-    question: "How can you add a comment in Javascript?",
+    question: "5. How can you add a comment in Javascript?",
     answers: ["<!--This is a comment-->", "//This is a comment", "'This is a comment", "This is a comment"]
 },
 {
-    question: "What is the correct way to write a Javascript array?",
+    question: "6. What is the correct way to write a Javascript array?",
     answers: ["let colors = 'red', 'green', 'blue'", "let colors = (1:'red', 2:'green', 3:'blue'", "let colors = ['red', 'green', 'blue'", "let colors = red, green, blue"]
 },
 {
-    question: "Which operator is used to assign a value to a variable?",
+    question: "7. Which operator is used to assign a value to a variable?",
     answers: ["=", "*", "-", "x"]
 },
 {
-    question: "What will the following code return: Boolean(10 > 9)",
+    question: "8. What will the following code return: Boolean(10 > 9)",
     answers: ["NaN", "True", "False", "None of the Above"]
 },
 {
-    question: "Which event occurs when the user clicks on an HTML element?",
+    question: "9. Which event occurs when the user clicks on an HTML element?",
     answers: ["onclick", "onmouseclick", "onmouseover", "onchange"]
 },
 {
-    question: "How do you round the number 7.25, to the nearest integer?",
+    question: "10. How do you round the number 7.25, to the nearest integer?",
     answers: ["round(7.25)", "Math.round(7.25)", "Math.rnd(7.25)", "rnd(7.25)"]
 }
 ];
@@ -52,8 +52,10 @@ questions.style.display = "block"
 function showQuestions() {
     questions.innerHTML = myTriviaQuestions[index].question
     for(let i = 0; i < myTriviaQuestions[index].answers.length; i++) {
-        choices.textContent = myTriviaQuestions[index].answers[i]
-    }
+        let btn = document.createElement("button")
+        btn.textContent = myTriviaQuestions[index].answers[i]
+        choices.appendChild(btn)
+    }index += 1;
 }
 
 startButton.addEventListener("click", function () {
