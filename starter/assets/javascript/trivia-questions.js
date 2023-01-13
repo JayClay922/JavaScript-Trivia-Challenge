24a19129a81fdb2f5342aec1bcbaa1819b0094b4
JayClay922 <jamesclayman922@live.com>
2023-01-13 16:07:18 +0000

    adding questions to code

---
+++ starter/assets/javascript/trivia-questions.js
@@ -0,0 +1,41 @@
+let myTriviaQuestions = [{
+    question: "Inside which HTML element do we put the Javascript?",
+    answers: ["<scripting>", "<script>", "<js>", "<javascript>"]           
+},
+{
+    question: "How do you write 'Hello World' in an alert box",
+    answers: ["alert('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", "msg('Hello World');"]
+},
+{
+    question: "How do you write an IF statement in Javascript?",
+    answers: ["if i == 5 then", "if i = 5", "if i = 5 then", "if(i == 5)"]
+},
+{
+    question: "How does a FOR loop start?",
+    answers: ["for i = 1 to 5", "for(i <= 5; i++", "for(i = 0; i <= 5)", "for(i = 0; i <= 5; i++)"]
+},
+{
+    question: "How can you add a comment in Javascript?",
+    answers: ["<!--This is a comment-->", "//This is a comment", "'This is a comment", "This is a comment"]
+},
+{
+    question: "What is the correct way to write a Javascript array?",
+    answers: ["let colors = 'red', 'green', 'blue'", "let colors = (1:'red', 2:'green', 3:'blue'", "let colors = ['red', 'green', 'blue'", "let colors = red, green, blue"]
+},
+{
+    question: "Which operator is used to assign a value to a variable?",
+    answers: ["=", "*", "-", "x"]
+},
+{
+    question: "What will the following code return: Boolean(10 > 9)",
+    answers: ["NaN", "True", "False", "None of the Above"]
+},
+{
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answers: ["onclick", "onmouseclick", "onmouseover", "onchange"]
+},
+{
+    question: "How do you round the number 7.25, to the nearest integer?",
+    answers: ["round(7.25)", "Math.round(7.25)", "Math.rnd(7.25)", "rnd(7.25)"]
+}
+]
